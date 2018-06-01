405324b38f972c7116c59449d2df88f7b115dc3f
Maxime Labelle <maxime.labelle@moskitos.fr>
2018-06-01 15:02:21 +0200

    Initial version.

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # clip
+
 A simple command-line replacement for the MS-DOS clip.exe tool.
+
+## Rationale
+
+I like to be able to clip one-liner into the Windows clipboard from a PowerShell prompt with a command like so:
+
+```PS
+PS> $PWD.Path | clip
+PS> [Guid]::NewGuid().guid | clip
+
+```
+
+However, when used this way, the CLIP command-line tool adds an extra line to the clipboard.
---
+++ src/clip/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace clip
+{
+    class Program
+    {
+        [STAThread]
+        static void Main(string[] args)
+        {
+            var builder = new StringBuilder();
+
+            string line = null;
+            while ((line = Console.ReadLine()) != null)
+                builder.AppendLine(line);
+
+            // remove extra lines
+
+            while (builder[builder.Length - 1] == '\r' || builder[builder.Length - 1] == '\n')
+                builder.Remove(builder.Length - 1, 1);
+
+            // copy to the clipboard
+
+            var text = builder.ToString();
+
+            Clipboard.SetText(text, TextDataFormat.UnicodeText);
+        }
+    }
+}
